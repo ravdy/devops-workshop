d1472b83fadefe60eb6ffc0791fda41c35f1990d
amarsyn <amarnathb1970@yahoo.com>
2023-07-22 15:31:14 +0530

    Committing V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
+    ami ="ami-04823729c75214919"
+    instance_type ="t2.micro"
+    key_name ="dpp"
 }
