0a987b6466c77bedb1dc89ebe975c49a428129b8
amarsyn <amarnathb1970@yahoo.com>
2023-07-25 16:15:33 +0530

    adding version 3 EC2 with VPC

---
+++ terraform_code/V3-EC2-with_VPC.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami ="ami-04823729c75214919"
+    instance_type ="t2.micro"
+    key_name ="dpp"
+    // security_groups = ["demo-sg"]
+    vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH access"
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
+
+resource = "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  Name ="dpp-vpc"
+}
+
+resource = "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id= aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dpp-public-subnet-01"
+  }
+}
+
+resource = "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id= aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "dpp-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = "aws_vpc.dpp-vpc.id"
+  tags = {
+    "Name" = "dpp-igw"
+  }
+}
+
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = "aws_vpc.dpp-vpc.id"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id
+  }
+}
+
+resource "aws_rt_table_association" "dpp-rta-public-subnet-01" {
+  subnet_id = "aws_subnet.dpp-public-subnet-01.id"
+  route_table_id = "aws_route_table.dpp-public-rt.id"
+}
+
+resource "aws_rt_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id = "aws_subnet.dpp-public-subnet-02.id"
+  route_table_id = "aws_route_table.dpp-public-rt.id"
+}
