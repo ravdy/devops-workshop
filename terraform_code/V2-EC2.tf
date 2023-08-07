cfbd200fe890bda4a7c1d66e3711e60518c68817
nikks.palyekar <nikks9.palyekar@gmail.com>
2023-08-07 13:02:44 +0530

    commiting v2-ec2.tf

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -3,34 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
-    instance_type = "t2.micro"
-    key_name = "dpp"
-    security_groups = [ "demo-sg" ]
+  ami = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name = "devops2"
+  security_groups = [ "demo-sg" ]
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
-  description = "SSH Access"
-  
+  description = "Allow ssh inbound traffic"
+
   ingress {
-    description      = "Shh access"
+    description      = "SSH Access"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    }
+  }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   tags = {
-    Name = "ssh-prot"
-
+    Name = "ssh-port"
   }
+}
 }
